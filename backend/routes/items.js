feaac76c1bf3e8321d24a653f12dc82a0547becd
Shahar47 <128405983+Shahar47@users.noreply.github.com>
2023-06-25 13:52:33 +0300

    update items

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const artworkController = require('../controllers/itemsController');
 
 
-router.post('/item/createitem', artworkController.createArtwork);
-router.post('/item/updateitem', artworkController.updateArtworks);
-router.post('/item/getitems', artworkController.getArtworks);
-router.get('/item/AllSales', artworkController.AllSales);
-router.get('/item/AvgSales', artworkController.AvgSales);
-router.get('/item/AllProfits', artworkController.AllProfits);
-router.get('/item/AvgProfits', artworkController.AvgProfits);
+router.post('/item/createitem', itemController.createItem);
+router.post('/item/updateitem', itemController.updateItems);
+router.post('/item/getitems', itemkController.getArtItems);
+router.get('/item/AllSales', itemController.AllSales);
+router.get('/item/AvgSales', itemController.AvgSales);
+router.get('/item/AllProfits', itemController.AllProfits);
+router.get('/item/AvgProfits', itemController.AvgProfits);
 
 
 module.exports = router;
