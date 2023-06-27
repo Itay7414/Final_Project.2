c5ce4e3231d0789596936ea0c2da92a322096911
Sagimo99 <mosacho1408@gmail.com>
2023-06-27 13:46:39 +0300

    ss

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -3,7 +3,7 @@ const cartControllers = require('../controllers/cartControllers');
 
 
 router.post('/carts/createCart', cartControllers.createCart);
-router.post('/carts/createCart', cartControllers.createCart);
+router.post('/carts/addToCart', cartControllers.addToCart);
 
 
 module.exports = router;
