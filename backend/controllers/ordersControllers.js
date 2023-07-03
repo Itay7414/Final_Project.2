e2d4ede9308b57e474ca5e7d0c69d6ea8e574104
Shahar47 <128405983+Shahar47@users.noreply.github.com>
2023-07-04 00:41:15 +0300

    ff

--- backend/controllers/ordersControllers.js
+++ backend/controllers/ordersControllers.js
@@ -48,28 +48,4 @@ exports.addToOrder = async (req, res) => {
         res.status(500).json({ message: 'Failed to add item to order' });
     }
 };
-function addToOrder(itemName, itemPrice) {
-    // Get the current order from the cookies
-    const orderCookie = Cookies.get('order');
-    let order = {};
 
-    if (orderCookie) {
-        // Parse the existing order from JSON
-        order = JSON.parse(orderCookie);
-    }
-
-    // Add the new item to the order or update its quantity
-    if (order[itemName]) {
-        order[itemName].quantity++;
-    } else {
-        order[itemName] = {
-            quantity: 1,
-            price: parseFloat(itemPrice),
-        };
-    }
-
-    // Store the updated order in the cookies
-    Cookies.set('order', JSON.stringify(order));
-
-    console.log('Item added to order:', order);
-}
