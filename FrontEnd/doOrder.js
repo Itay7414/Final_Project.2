8863e94c4e2da6c6836c9a0650e97212e0e87535
Itay7414 <itaykoko7414@gmail.com>
2023-07-03 21:16:30 +0300

    :(

--- FrontEnd/doOrder.js
+++ FrontEnd/doOrder.js
@@ -1,48 +1,53 @@
 const addToOrder = document.querySelectorAll('.add-to-order-btn');
 addToOrder.forEach(button => {
-    button.addEventListener('click', () => {
-        const fruitName = button.closest('.card').querySelector('.item-name').textContent;
-        const quantityInput = button.closest('.card').querySelector('.item-amount');
-        const quantity = parseFloat(quantityInput.value);
-        const price = parseFloat(button.closest('.card').querySelector('.item-price').textContent.split('$')[1]);
+  button.addEventListener('click', () => {
+    const fruitName = button.closest('.card').querySelector('.item-name').textContent;
+    const quantityInput = button.closest('.card').querySelector('.item-amount');
+    const quantity = parseFloat(quantityInput.value);
+    const price = parseFloat(button.closest('.card').querySelector('.item-price').textContent.split('$')[1]);
 
-        // Make AJAX request to addToOrder route
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/orders/addToOrder', true);
-        xhr.withCredentials = true; // Include cookies in the request
-        xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.setRequestHeader('Authorization', 'Bearer your-authentication-token');
+    // Make AJAX request to addToOrder route
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/orders/addToOrder', true);
+    xhr.withCredentials = true; // Include cookies in the request
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Bearer your-authentication-token');
 
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                const orderData = JSON.parse(xhr.responseText);
-                updateOrderTable(orderData);
-            }
-        };
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const orderData = JSON.parse(xhr.responseText);
+        updateOrderTable(orderData);
+      }
+    };
 
-        const data = JSON.stringify({ fruitName, quantity, price });
-        xhr.send(data);
-    });
+    const data = JSON.stringify({ fruitName, quantity, price });
+    xhr.send(data);
+  });
 });
 
 
 // Function to update the order table in the orders.ejs file
 function updateOrderTable(orderData) {
-    // Get the table body element
-    const tableBody = document.getElementById('order-table-body');
+  // Get the table body element
+  const tableBody = document.getElementById('order-table-body');
 
-    // Create a new row for the order data
+  // Clear the table body before updating
+  tableBody.innerHTML = '';
+
+  // Loop through each item in the order data
+  orderData.items.forEach(item => {
+    // Create a new row for the item
     const newRow = document.createElement('tr');
 
-    // Create table cells for the order data
+    // Create table cells for the item data
     const nameCell = document.createElement('td');
-    nameCell.textContent = orderData.name;
+    nameCell.textContent = item.name;
 
     const quantityCell = document.createElement('td');
-    quantityCell.textContent = orderData.quantity;
+    quantityCell.textContent = item.quantity;
 
     const priceCell = document.createElement('td');
-    priceCell.textContent = orderData.price;
+    priceCell.textContent = item.price;
 
     // Append the cells to the new row
     newRow.appendChild(nameCell);
@@ -51,7 +56,8 @@ function updateOrderTable(orderData) {
 
     // Append the new row to the table body
     tableBody.appendChild(newRow);
+  });
 
-    // Example: Log the order data to the console
-    console.log(orderData);
+  // Example: Log the order data to the console
+  console.log(orderData);
 }
--- backend/controllers/ordersControllers.js
+++ backend/controllers/ordersControllers.js
@@ -1,18 +1,17 @@
 const models = require('../utils/db_utils/models');
 const Order = models.Order;
 
-
 exports.addToOrder = async (req, res) => {
     try {
         console.log('Start addtocart');
         const username = req.cookies.user.username;
         const fruitName = req.body.fruitName;
         const quantity = parseFloat(req.body.quantity);
+        const price = parseFloat(req.body.price);
         console.log('userid: ', username);
 
         if (!username) {
             console.log('not exist addtocart');
-
             return res.status(401).json({ message: 'User not authenticated' });
         }
 
@@ -29,6 +28,7 @@ exports.addToOrder = async (req, res) => {
 
         // Add the item to the order
         order.items.push({
+            type: 'fruit',  // Add the type field
             name: fruitName,
             price: price,
             quantity: quantity
@@ -42,7 +42,6 @@ exports.addToOrder = async (req, res) => {
         res.status(200).json(updatedOrder);
     } catch (err) {
         console.error('Failed to add item to order:', err);
-
         res.status(500).json({ message: 'Failed to add item to order' });
     }
 };
