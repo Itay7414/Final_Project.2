0ee9acf5c1b82f000763daa5aa0a5a915ef3c4ca
Itay7414 <itaykoko7414@gmail.com>
2023-07-04 09:33:21 +0300

    recognize if the user is logged in and connect him to the cart/sign in

--- FrontEnd/doOrder.js
+++ FrontEnd/doOrder.js
@@ -2,28 +2,42 @@
 
 async function addToOrder(fruitName, price, quantity) {
   try {
-      const response = await fetch('/orders/addToOrder', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              fruitName,
-              price,
-              quantity,
-          }),
-          credentials: 'same-origin', // Include cookies in the request
-      });
+    if (quantity < 1) {
+      alert("Minimum quantity is 1 :)");
+      return;
+    }
 
-      if (response.ok) {
-          // Handle a successful response, if needed
-          console.log('Item added to order successfully');
+    const response = await fetch('/orders/addToOrder', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fruitName,
+        price,
+        quantity,
+      }),
+      credentials: 'same-origin', // Include cookies in the request
+    });
+
+    if (response.ok) {
+      const updatedOrder = await response.json();
+      alert("Added to the cart!");
+
+      // Update the cart display if needed
+      // For example, you can call a function to update the cart UI
+      updateCartDisplay(updatedOrder);
+    } else {
+      const errorData = await response.json();
+      if (response.status === 401) {
+        alert("You have to log in to your user before adding items to the cart!");
       } else {
-          // Handle an error response, if needed
-          console.log('Failed to add item to order');
+        // Handle other error responses, if needed
+        console.log('Failed to add item to order:', errorData.message);
       }
+    }
   } catch (error) {
-      // Handle any error that occurred during the request
-      console.error('Error adding item to order:', error);
+    // Handle any error that occurred during the request
+    console.error('Error adding item to order:', error);
   }
 }
